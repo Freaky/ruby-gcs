8f83d9bd41535af264f2d066c8c5c3a2f69d33e4
Thomas Hurst <tom@hur.st>
2018-04-14 23:53:39 +0100

    GCS::Reader: Use index on-disk if it's got more than 1024 entries.
    A pwned-passwords-2.0.gcs default index has about half a million, so
    this saves a fair bit of memory.

--- lib/gcs/reader.rb
+++ lib/gcs/reader.rb
@@ -8,8 +8,70 @@ module GCS
     attr_reader :n
     attr_reader :p
 
-    def inspect
-      "#<#{self.class}:0x#{self.object_id.to_s(16)} n=#{@n} p=#{@p} indexsize=#{@index.size} io=#{@io.inspect}"
+    module Index
+      class OnDisk
+        RECORDSIZE = 16
+
+        attr_reader :length
+
+        def initialize(io, start, length)
+          @io = io
+          @start = start
+          @length = length
+        end
+
+        def lookup(key)
+          low = 0
+          high = @length # -1 ?
+          midkey, midval = 0
+          mid = low + high / 2
+
+          while low <= high
+            mid = (low + high) / 2
+
+            # Replace with pread
+            @io.seek(@start + (mid * RECORDSIZE))
+            midkey, midval = @io.read(RECORDSIZE).unpack('Q>2')
+
+            if midkey < key
+              low = mid + 1
+            elsif midkey > key
+              high = mid - 1
+            else
+              break
+            end
+          end
+
+          @io.seek([@start + (mid * RECORDSIZE) - RECORDSIZE, @start].max)
+          return @io.read(RECORDSIZE).unpack('Q>2')
+        end
+      end
+
+      class InMemory
+        attr_reader :length
+
+        def initialize(io, start, length)
+          @length = length
+          io.seek(start)
+          @index = [[0,0]]
+          @index += Array.new(length) do
+            io.read(16).unpack('Q>2')
+          end
+        end
+
+        def lookup(key)
+          idx = @index.bsearch_index { |x| x[0] >= key }
+          if idx
+            @index[[idx - 1, 0].max]
+          else
+            @index.last
+          end
+        end
+
+       def inspect
+         "#<#{self.class}:0x#{self.object_id.to_s(16)} start=#{@start} length=#{@length}>"
+       end
+      end
     end
 
     def initialize(io)
@@ -22,20 +84,24 @@ module GCS
       @n, @p, @end_of_data, @index_len = footer.unpack('Q>4')
       @log2p = Math.log2(@p).ceil
 
-      io.seek(@end_of_data)
-      @index = [[0,0]]
-      @index += Array.new(@index_len) do
-        io.read(16).unpack('Q>2')
+      indeximpl = case @index_len
+      when 0..1024 then Index::InMemory
+      else              Index::OnDisk
       end
+
+      @index = indeximpl.new(io, @end_of_data, @index_len)
 
       @io = BitReader.new(io)
     end
 
     def exists?(key)
       h = key % (@n * @p)
-      idx = [@index.bsearch_index { |x| x[0] >= h } - 1, 0].max
 
-      last, pos = @index[idx]
+      last = pos = 0
+
+      if lp = @index.lookup(h)
+        last, pos = lp
+      end
 
       @io.seek(pos)
 
